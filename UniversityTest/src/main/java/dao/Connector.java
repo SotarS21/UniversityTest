cd786eb2e9ba3c9f0c1b7035787ca1047d6f0644
Arthemise jonas <arthemise.jonas@gmail.com>
2017-03-28 13:56:11 +0200

    [add DAO] ajout de la dao générique

---
+++ UniversityTest/src/main/java/dao/Connector.java
@@ -0,0 +1,121 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Connector {
+
+	private  static EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-pu");
+	private static EntityManager em ;
+	
+	
+	private static class Holder {
+        static final Connector INSTANCE = new Connector();
+    }
+
+    public static Connector getInstance() {
+        return Holder.INSTANCE;
+    } 
+	
+	public void connection()
+	{
+		em = emf.createEntityManager();
+		
+	}
+	
+	
+	public <T> void AddObject(T obj)
+	{
+		
+		
+		try
+		{
+			Connector.em.getTransaction().begin();
+		
+			Connector.em.persist(obj);
+
+			Connector.em.getTransaction().commit();
+		}
+		catch (Exception e) {
+			if (Connector.em != null)
+				Connector.em.getTransaction().rollback();
+			e.printStackTrace();
+		}finally {
+			
+		}
+	}
+	
+	public <T> void removeObject(T obj)
+	{
+		
+		
+		try
+		{
+			Connector.em.getTransaction().begin();
+		
+			Connector.em.remove(obj);
+
+			Connector.em.getTransaction().commit();
+		}
+		catch (Exception e) {
+			if (Connector.em != null)
+				Connector.em.getTransaction().rollback();
+			e.printStackTrace();
+		}finally {
+		}
+	}
+	public <T> T getElementById(Class<T> classType, long id)
+	{
+		T ret = null;
+		
+		try
+		{
+			Connector.em.getTransaction().begin();
+		
+		ret = (T) em.find(classType, id);
+
+		Connector.em.getTransaction().commit();
+		}
+		catch (Exception e) {
+			if (Connector.em != null)
+				Connector.em.getTransaction().rollback();
+			e.printStackTrace();
+		}finally {
+			
+		}
+		return ret;
+	}
+	
+	public <T> void updateObj(T obj)
+	{
+
+		try
+		{
+			Connector.em.getTransaction().begin();
+		
+			Connector.em.merge(obj);
+
+			Connector.em.getTransaction().commit();
+		}
+		catch (Exception e) {
+			if (Connector.em != null)
+				Connector.em.getTransaction().rollback();
+			e.printStackTrace();
+		}finally {
+			
+		}
+	}
+	
+	
+	public void deconnection()
+	{
+		if (em != null)
+			Connector.em.close();
+		if (emf != null)
+			emf.close();
+		
+	}
+	
+	
+}
